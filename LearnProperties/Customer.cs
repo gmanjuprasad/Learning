18c21aaab3d4b2eeff9a3a0a9f28b0b5da1af92d
Manju Prasad G <gmanjuprasad@outlook.com>
2018-07-20 00:14:44 +0530

    Learn Properties

---
+++ LearnProperties/Customer.cs
@@ -0,0 +1,37 @@
+﻿namespace LearnProperties
+{
+    public class Customer
+    {
+        public Customer()
+        {
+                
+        }
+
+        private int _id;
+        private string _firstname;
+        private string _lastname;
+
+        public int ID
+        {
+            get { return this._id; }
+            set { this._id = value;  }
+        }
+
+        public string FirstName
+        {
+            get { return this._firstname; }
+            set { this._firstname = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastname; }
+            set { this._lastname = value; }
+        }
+
+        public string FullName
+        {
+            get { return this._firstname + " " + this._lastname; }
+        }
+    }
+}
---
+++ LearnProperties/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LearnProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer cus = new Customer()
+            {
+                ID = 1,
+                FirstName = "Manju",
+                LastName = "Prasad",
+            };
+
+            
+            Console.WriteLine("The Cutomer Full Name is " + cus.FullName + "");
+        }
+    }
+}
